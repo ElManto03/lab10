d4705e6af7bce135848928fbae30d5ac2a17061a
Federico Mantoni - federico.mantoni@studio.unibo.it <federico.mantoni@studio.unibo.it>
2023-11-28 16:46:12 +0100

    Done exercise 101, build successful

--- 101-lambda-utilities-for-lists-and-maps/build.gradle.kts
+++ 101-lambda-utilities-for-lists-and-maps/build.gradle.kts
@@ -19,7 +19,10 @@ dependencies {
 
     // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.1") // Use the latest version
 }
+
 
 val mainClass: String by project
 
